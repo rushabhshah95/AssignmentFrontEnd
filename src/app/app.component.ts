11dbd6e28342f72af4d8dbf936176aabff4bf71c
rushabhshah95 <russhah95@gmail.com>
2021-03-23 22:26:01 +0530

    frontend code

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarService } from './nav-bar/navbar.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
